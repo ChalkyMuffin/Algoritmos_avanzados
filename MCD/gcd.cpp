ef32bacebfc426e964fb0b0a9026728d8281df7e
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-08-16 15:29:38 -0600

    gcd jala

--- MCD/gcd.cpp
+++ MCD/gcd.cpp
@@ -1,10 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <cmath>
 #include <iostream>
 using namespace std;
+
+int gcd(int n, int m)
+{
+    if (n == 0)
+        return m;
+    return gcd( m % n, n);
+}
+ 
+
 int main ()
 {
+    int n = 60;
+    int m = 24;
+    cout << gcd(n,m);
+
     return 0;
 }
