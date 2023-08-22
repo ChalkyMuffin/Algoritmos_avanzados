67ae7ffe524b31bd351d411d548aecc001429767
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-08-22 17:27:42 -0600

    a^n mas comentado

--- a^n/a^n.cpp
+++ a^n/a^n.cpp
@@ -5,6 +5,12 @@ using std::endl;
 
 //Complejidad O(log n)
 
+
+/*Es mas eficiente que con el metodo de fuerza bruta porque con fuerza bruta su complejidad seria O(n) ya que estaria multiplicando el numero 
+las n veces que el exponente diga mientras que con divide and conquer su complejidad seria < n pues seria O(log n) 
+ya que este otro usa la recursividad y la division del problema para llegar al mismo resultado con menos iteraciones*/
+
+
 //Funcion a la que le das un numero y la potencia a la que lo quieres y hace la operacion de forma recursiva siguiendo divide y venceras
 //Input: Dos numeros, uno que sera afectado y otro al que vas a usar para que sea la potencia de este
 //Regresa: El numero a la potencia del otro
