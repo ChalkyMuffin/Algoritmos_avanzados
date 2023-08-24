f8c40abb704198ae36ad13bd26512dc4aef7f157
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-08-24 14:29:37 -0600

    stair-climbing jalando

--- Stair-climbing/stairClimbing.cpp
+++ Stair-climbing/stairClimbing.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
 using namespace std;
 
-//Tenemos una escalera de 𝑛 peldaños
-//Se puede subir en pasos de 1 o 2 peldaños a la vez 
-//¿De cuantas formas diferentes se puede llegar hasta arriba? 
-//a = 1 por el caso de dar 1 salto
-//b = 2 por el caso de dar 2 saltos
-int stairs(int n, int a, int b){
-    
-    
-    if (n <= 0){
-        return 0;
-    }
-
-    else if(n == 1 || n == 2){
-        return n;
-    }
-
-    else{
-        return stairs(n-1, b, a+b);
-    }
-
-}
-
-int main ()
+// Tenemos una escalera de 𝑛 peldaños
+// Se puede subir en pasos de 1 o 2 peldaños a la vez
+// ¿De cuantas formas diferentes se puede llegar hasta arriba?
+// a = 1 por el caso de dar 1 salto
+// b = 2 por el caso de dar 2 saltos
+int stairs(int n, int a, int b)
 {
 
-    cout << stairs(5, 1, 2);
+    if (n == 1)
+    {
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "n = 1" << endl;
+        return a;
+    }
+
+    else if (n == 2)
+    {
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        cout << "n = 2" << endl;
+        return b;
+    }
+
+    else
+    {
+        cout << "a = " << a << endl;
+        cout << "b = " << b << endl;
+        return stairs(n - 1, b, a + b);
+    }
+}
+
+int main()
+{
+
+    cout << stairs(4, 1, 2);
     return 0;
 }
