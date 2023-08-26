0b774a3cc5346c5dc9198d38f1ac83ca26225dd4
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-08-25 20:42:58 -0600

    Mas como la explicacion

--- coin-collecting/coinCollecting.cpp
+++ coin-collecting/coinCollecting.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
-int C[5][5]={{0, 0, 0, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 0, 1,0}, {0, 0, 1, 0,0}, {1,0,0,0,1}};
+//Se crea la matriz que simula el camino representando los espacion vacios con 0 y los espacios con monedas con 1
+int C[5][5]={{0, 0, 0, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 0, 1, 0}, {0, 0, 1, 0,0}, {1, 0, 0, 0, 1}};
 
 int F(int i, int j) {
-    if (i == 0 && j == 0) {
-        return C[i][j];
+    if (i >= 0 && j >= 0) {
+        return max(F(i - 1, j), F(i, j - 1)) + C[i][j];
+        
     }
     if (i < 0 || j < 0) {
+        //cout << "xd" << endl;
         return 0;
     }
-    return max(F(i - 1, j), F(i, j - 1)) + C[i][j];
+    return 0;
 }
 
 int main ()
